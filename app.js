5dcb342ff81f6065a6470e8242229cc505805681
tris43210 <tristan.g.king@gmail.com>
2025-09-12 16:03:49 +0100

    appended images using forEach

--- app.js
+++ app.js
@@ -7,22 +7,14 @@ const imageArray = [
   { src: `./resources/Pink3dRender.webp`, alt: `Impressive Pink 3d Render` },
 ];
 
-// Basic Functionality of appending images to the container
-
-function createGreenImg() {
-  const greenImg = document.createElement(`img`);
-  greenImg.src = imageArray[0].src;
-  imageContainer.appendChild(greenImg);
+function createThumbnail() {
+  imageArray.forEach(function (image) {
+    let createImg = document.createElement(`img`);
+    createImg.src = image.src;
+    imageContainer.appendChild(createImg);
+  });
 }
 
-function createPinkImg() {
-  const pinkImg = document.createElement(`img`);
-  pinkImg.src = imageArray[1].src;
-  imageContainer.appendChild(pinkImg);
-}
-
-createGreenImg();
-createPinkImg();
-
+console.log(createThumbnail());
 console.log(imageArray[0]);
 console.log(imageContainer);
